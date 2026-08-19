78b35f16997a274dcb0603ba3e61c58a12442cfd
dhumal sanika <dhumalsanika34@gmail.com>
2026-08-20 00:25:57 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-import react from 'react'
-function App(){
-  return (
-    <div>
-      <h1>BloodCare</h1>
-    </div>
-  )
+import React from "react";
+import Home from "./views/Home/Home";
+import About from "./views/About/About";
+import Contact from "./views/Contact/Contact";
+
+function App() {
+  const path = window.location.pathname;
+  console.log["current Path means current route:", path];
+  if (path === "/") {
+    return (
+      <>
+       
+        <Home />
+        
+      </>
+    );
+  }
+  else if (path === "/about") {
+    return (
+      <>
+      
+        <About />
+       
+      </>
+    );
+  }
+  else if (path === "/contact") {
+    return (
+      <>
+       
+        <Contact />
+        
+      </>
+    );
+  }
+
 }
 export default App
