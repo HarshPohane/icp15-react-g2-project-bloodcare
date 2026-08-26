51dd3bca7e53cb10806a4d9be38e3599998b4c07
dhumal sanika <dhumalsanika34@gmail.com>
2026-08-26 17:10:25 +0530

    import the campigns file.

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/NavBar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+
+import BloodBanks from "./views/BloodBanks/BloodBanks";
+import BloodRequest from "./views/BloodRequest/BloodRequest";
+import Campaigns from "./views/Campaigns/Campaigns";
+
+import FindBlood from "./views/FindBlood/FindBlood";
+import Hospitals from "./views/Hospitals/Hospitals";
+import Login from "./views/Login/Login";
+import Signup from "./views/Signup/Signup";
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+
+
+
+      <Route path="/bloodbanks" element={<BloodBanks />} />
+      <Route path="/bloodrequest" element={<BloodRequest />} />
+      <Route path="/campaigns" element={<Campaigns />} />
+
+      <Route path="/find-blood" element={<FindBlood />} />
+      <Route path="/hospitals" element={<Hospitals />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+    </Routes>
+    <Footer />
+  </BrowserRouter>,
+
+)
+
